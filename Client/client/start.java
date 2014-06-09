f9489cf1277b55dfe5248dfca3688efea16d10fc
Isaac Wheeler <isaacwheeler0@gmail.com>
2014-06-09 07:57:09 -0400

    update

--- Client/client/start.java
+++ Client/client/start.java
@@ -13,7 +13,7 @@ import common.InstallInfo;
 import common.StreamUtils;
 
 public class start {
-	
+
 	public static void cstart(String mod, String version, boolean preview)
 			throws Exception {
 		System.out.println("test client");
@@ -22,51 +22,53 @@ public class start {
 		System.out.println(dir);
 		// Forgeinstall.forge(false, common.Main.ForgePath);
 		File AppPath = GetApplicationPath.AppPath();
-		
+
 		File PAQ164 = new File(AppPath.toString() + "/PAQ/Instances/PAQ");
 
-		if(PAQ164.exists()){
+		if (PAQ164.exists()) {
 			PAQ164.renameTo(new File(AppPath.toString() + "/PAQ/OldPAQ1.6.4"));
-			FileUtils.DelateDirectory(new File(AppPath.toString() + "/PAQ/Installer"));
-			FileUtils.DelateDirectory(new File(AppPath.toString() + "/PAQ/Launcher"));
-			FileUtils.DelateDirectory(new File(AppPath.toString() + "/PAQ/Instances"));
+			FileUtils.DelateDirectory(new File(AppPath.toString()
+					+ "/PAQ/Installer"));
+			FileUtils.DelateDirectory(new File(AppPath.toString()
+					+ "/PAQ/Launcher"));
+			FileUtils.DelateDirectory(new File(AppPath.toString()
+					+ "/PAQ/Instances"));
 		}
-		
-		
+
 		File PAQ17X = new File(AppPath.toString() + "/PAQ/PAQ1.7.X");
-		if(!PAQ17X.exists()){
+		if (!PAQ17X.exists()) {
 			PAQ17X.mkdir();
 		}
-		
+
 		File Mods17X = new File(PAQ17X.toString() + "/mods");
-		if(!Mods17X.exists()){
+		if (!Mods17X.exists()) {
 			Mods17X.mkdir();
-		}else{
+		} else {
 			FileUtils.DelateDirectory(Mods17X);
 			Mods17X.mkdir();
 		}
-		
+
 		File Config17X = new File(PAQ17X.toString() + "/config");
-		if(!Config17X.exists()){
+		if (!Config17X.exists()) {
 			Config17X.mkdir();
-		}else{
+		} else {
 			FileUtils.DelateDirectory(Config17X);
 			Config17X.mkdir();
 		}
-		
-		InstallInfo obj = GetInstallInfo.JsonInfo("http://magetech.no-ip.org/test.json");
-		
-		File tmp = File.createTempFile("Config", ".jar");
-        tmp.deleteOnExit();
-        try(InputStream is = new URL(obj.Config()).openStream()) {
-            StreamUtils.saveTo(is, tmp);
-        }
-        
-        FileUtils.unzip(tmp.toString(), Config17X.toString());
-		
-	}
 
+		InstallInfo obj = GetInstallInfo
+				.JsonInfo("http://magetech.no-ip.org/test.json");
+
+		File tmp = File.createTempFile("Config", ".jar");
+		tmp.deleteOnExit();
+		try (InputStream is = new URL(obj.Config()).openStream()) {
+			StreamUtils.saveTo(is, tmp);
+		}
+
+		FileUtils.unzip(tmp.toString(), Config17X.toString());
+
+	}
 
 }
 
-//Users/IsaacWheeler/Library/Application Support/PAQ/Instances/PAQ
+// Users/IsaacWheeler/Library/Application Support/PAQ/Instances/PAQ
